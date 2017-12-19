d1b56670968b1605076b467b1637e788cb7fca22
Jason <vivanaranja@gmail.com>
2017-12-18 20:49:28 -0800

    html routes

--- server.js
+++ server.js
@@ -1,4 +1,30 @@
+// Dependencies
+// ===========================================================
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Data
+// ===========================================================
+
+
+
+
+
+
+
+
+
+
+// Listener
+// ===========================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
