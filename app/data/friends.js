be93ff3d19f84736d27be60db68f8ae8c1c027f1
Jason <vivanaranja@gmail.com>
2017-12-24 13:10:09 -0800

    apiRoutes still needs nested loop

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,7 @@
 // DATA
 // ===============================================================================
 
-var friendArray = [
+var friendsData = [
     {
         'name': 'Myles',
         'photo': 'https://www.buildabear.com/dw/image/v2/BBNG_PRD/on/demandware.static/-/Sites-buildabear-master/default/dw02c1fb63/23342x.jpg?sw=600&sh=600&sm=fit',
@@ -23,4 +23,4 @@ var friendArray = [
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
-module.exports = friendArray;
+module.exports = friendsData;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,47 +1,44 @@
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
+// Require express, path and require list of friends data.
 // ===============================================================================
-
-var friendsData = require("../data/friends");
 var express = require('express');
+var path = require('path');
+var friendsData = require('../data/friends');
 var router = express.Router();
 
 // ===============================================================================
 // ROUTING
 // ===============================================================================
 
+module.exports = function(app) {
 
-
-module.exports = function (app) {
     // API GET Requests
     // Below code handles when users "visit" a page.
     // In each of the below cases when a user visits a link
     // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // List of friend data.
     // ---------------------------------------------------------------------------
 
     app.get("/api/friends", function (req, res) {
         res.json(friendsData);
     });
 
-    // app.get("/api/waitlist", function (req, res) {
-    //     res.json(waitListData);
-    // });
 
     // API POST Requests
     // Below code handles when a user submits a form and thus submits data to the server.
+    // Adds new friend to the friend data array.
     // In each of the below cases, when a user submits form data (a JSON object)
     // ...the JSON is pushed to the appropriate JavaScript array
-    // (ex. User fills out a reservation request... this data is then sent to the server...
-    // Then the server saves the data to the tableData array)
+    // Then the server saves the data to the friendsData array)
     // ---------------------------------------------------------------------------
 
     app.post("/api/friends", function (req, res) {
        console.log(req.body);
        var userData = req.body;
         // req.body is available since we're using the body-parser middleware
-        // loop through frineds data array and compare surveyResults 
+        // loop through friends data array and compare surveyResults 
         // with each friend data entry
 
         for(var i=0; i < friendsData.length; i++) {
