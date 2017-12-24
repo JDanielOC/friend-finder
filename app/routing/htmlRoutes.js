6191f5897ceb310bbe301b65dd356a9d775d3c50
Jason <vivanaranja@gmail.com>
2017-12-24 13:02:19 -0800

    html routes complete

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -9,20 +9,20 @@ var router = express.Router();
 // ===============================================================================
 
 module.exports = function (router) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-router.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-});
+    // HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+    // ---------------------------------------------------------------------------
+    router.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 
-router.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
-});
+    router.get("/survey", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
+    });
 
     // If no matching route is found default to home
-    router.get("*", function (req, res) {
+    router.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 };
