6687f5ed10aca5c6d5622f4545fa9f6f7b35a1b2
Jason <vivanaranja@gmail.com>
2017-12-24 12:51:27 -0800

    add that comma to the array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,11 @@
 // ===============================================================================
 // DATA
-// Below data will hold all of the reserved tables.
-// Initially we just set it equal to a "dummy" customer.
-// But you could have it be an empty array as well.
 // ===============================================================================
 
 var friendArray = [
     {
         'name': 'Myles',
-        'photo': 'https://www.buildabear.com/dw/image/v2/BBNG_PRD/on/demandware.static/-/Sites-buildabear-master/default/dw02c1fb63/23342x.jpg?sw=600&sh=600&sm=fit'
+        'photo': 'https://www.buildabear.com/dw/image/v2/BBNG_PRD/on/demandware.static/-/Sites-buildabear-master/default/dw02c1fb63/23342x.jpg?sw=600&sh=600&sm=fit',
         'scores': [
             '1',
             '2',
