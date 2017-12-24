bd3555d1c2865122de637b81d6750a1ae5aa8860
Jason <vivanaranja@gmail.com>
2017-12-24 12:49:06 -0800

    friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,9 +7,9 @@
 
 var friendArray = [
     {
-        name: 'Myles',
-        photo: 'https://www.buildabear.com/dw/image/v2/BBNG_PRD/on/demandware.static/-/Sites-buildabear-master/default/dw02c1fb63/23342x.jpg?sw=600&sh=600&sm=fit'
-        scores: [
+        'name': 'Myles',
+        'photo': 'https://www.buildabear.com/dw/image/v2/BBNG_PRD/on/demandware.static/-/Sites-buildabear-master/default/dw02c1fb63/23342x.jpg?sw=600&sh=600&sm=fit'
+        'scores': [
             '1',
             '2',
             '4',
