972388a190845161c763ed3588f05a5e6f0b6ade
Jason <vivanaranja@gmail.com>
2017-12-24 13:24:17 -0800

    add a friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -19,6 +19,23 @@ var friendsData = [
             '2'
         ]
         
+    },
+    {
+        'name': 'Brenda',
+        'photo': 'https://www.buildabear.com/dw/image/v2/BBNG_PRD/on/demandware.static/-/Sites-buildabear-master/default/dw01c458c3/24590x.jpg?sw=600&sh=600&sm=fit',
+        'scores': [
+            '3',
+            '2',
+            '4',
+            '2',
+            '2',
+            '3',
+            '5',
+            '1',
+            '4',
+            '1'
+        ]
+
     }
 ];
 
